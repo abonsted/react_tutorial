686bf2a85f965f384bf78ee7ac562d13b4cffc5b
abonsted <abonsted@vols.utk.edu>
2023-02-19 13:53:02 -0500

    intro to react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import { useState, useEffect } from 'react';
 
 const Person = (props) => {
   return(
@@ -12,10 +12,20 @@ const Person = (props) => {
 }
 
 const App = () => {
+  const [counter,setCounter] = useState(0);
+  
+  useEffect(() => {
+    //alert("You've changed the counter to " + counter); ... [counter]
+    setCounter(100);
+  }, [])
+
   return (
     <div className="App">
       <Person name={'John'} lastName={'Doe'} age={25}/>
       <Person name='Andrew' lastName='Bonsted' age={19}/>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
